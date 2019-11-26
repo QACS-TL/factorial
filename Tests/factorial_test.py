605cdba8b740e9b15b7ff8cd0a2f9fb3649162a6
PCMBarber <Kochab.96@gmail.com>
2019-11-26 15:57:25 +0000

    testing jenkins

--- Tests/factorial_test.py
+++ Tests/factorial_test.py
@@ -4,8 +4,16 @@ from Not_tests import notimportant
 def test_fact():
     assert notimportant.fact(5)==120
 
-def test_revers_fact():
+def test_reverse_fact1():
     assert notimportant.reverse_fact(120)=="5!"
 
-def test_revers_fact():
+def test_reverse_fact2():
     assert notimportant.reverse_fact(3628800)=="10!"
+
+def test_reverse_fact3():
+    assert notimportant.reverse_fact(5040)=="7!"
+
+def test_reverse_fact4():
+    assert notimportant.reverse_fact(720)=="6!"
+
+    
